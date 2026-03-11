a42bf9c67cff8f588ab440cdb73b7e024f63e12a
vladkuz54 <vkuz1540@gmail.com>
2026-03-11 19:57:29 +0200

    add api for swagger

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from presentation.routers import course_router, module_router, material_router
+from presentation.routers import api_router
 
 app = FastAPI()
 templates = Jinja2Templates(directory="presentation/templates")
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-app.include_router(course_router.router)
-app.include_router(module_router.router)
-app.include_router(material_router.router)
+app.include_router(course_router.router, include_in_schema=False)
+app.include_router(module_router.router, include_in_schema=False)
+app.include_router(material_router.router, include_in_schema=False)
+app.include_router(api_router.router)
